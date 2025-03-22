e7a3c2dd81718688f1eafa1ce244c6e33f2fdbc8
serhii <admin@admin.com>
2025-03-22 17:21:13 +0100

    Added documentation in schemas.py

--- schemas.py
+++ schemas.py
@@ -1,31 +1,37 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 from typing import List, Optional
 
 class CharacteristicBase(BaseModel):
-    name: str
-    value: str
+    name: str = Field(..., title="Name of characteristic", description="Colour or weight")
+    value: str = Field(..., title="Value of characteristic", description="Black, white, etc")
 
 class CharacteristicCreate(CharacteristicBase):
     pass
 
 class Characteristic(CharacteristicBase):
-    id: int
-    product_id: int
+    id: int = Field(..., title="ID of characteristic")
+    product_id: int = Field(..., title="ID of required product")
 
     class Config:
         orm_mode = True
 
 class ProductBase(BaseModel):
-    name: str
-    quantity: int
-    sku: str
+    name: str = Field(..., title="Name of product", description="Example: Laptop, toy, car, etc")
+    quantity: int = Field(..., title="Quantity of product", description="How much of this product we have")
+    sku: str = Field(..., title="SKU of product", description="Unique identifier for this product")
 
 class ProductCreate(ProductBase):
-    characteristics: List[CharacteristicCreate] = []
+    characteristics: List[CharacteristicCreate] = Field(
+        default=[], title="Characteristics of product",
+        description="Additional info about product"
+    )
 
 class Product(ProductBase):
-    id: int
-    characteristics: List[Characteristic] = []
+    id: int = Field(..., title="ID of product", description="Unique identifier for this product")
+    characteristics: List[Characteristic] = Field(
+        default=[], title="Characteristics of product",
+        description="Additional info about product"
+    )
 
     class Config:
         orm_mode = True
