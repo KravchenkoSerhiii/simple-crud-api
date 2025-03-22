09ad0b411bb1e1e39de24d22ee1f0e0f92468d16
serhii <admin@admin.com>
2025-03-22 18:09:11 +0100

    Some changes for correct connection to db

--- database.py
+++ database.py
@@ -5,7 +5,7 @@ from sqlalchemy.orm import sessionmaker
 
 Base = declarative_base()
 
-ssl_context = ssl.create_default_context(cafile="/Users/macbook/Desktop/eu-central-1-bundle.pem")
+ssl_context = ssl.create_default_context(cafile="/certs/eu-central-1-bundle.pem")
 
 DATABASE_URL = "postgresql+asyncpg://fastapi_user:lolkek17@fastapi-db.cdiy4my8iw6o.eu-central-1.rds.amazonaws.com:5432/fastapi-db"
 
