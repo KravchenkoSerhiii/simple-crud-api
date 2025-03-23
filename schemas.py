3f3bad6f8df7a3b6b2b675145252df8322792f02
serhii <admin@admin.com>
2025-03-23 11:55:14 +0100

    Added examples to documentation

--- schemas.py
+++ schemas.py
@@ -2,35 +2,70 @@ from pydantic import BaseModel, Field
 from typing import List, Optional
 
 class CharacteristicBase(BaseModel):
-    name: str = Field(..., title="Name of characteristic", description="Colour or weight")
-    value: str = Field(..., title="Value of characteristic", description="Black, white, etc")
+    name: str = Field(
+        ...,
+        title="Name of characteristic",
+        description="Colour or weight",
+        example="Colour"
+    )
+    value: str = Field(
+        ...,
+        title="Value of characteristic",
+        description="Black, white, etc",
+        example="Black"
+    )
 
 class CharacteristicCreate(CharacteristicBase):
     pass
 
 class Characteristic(CharacteristicBase):
-    id: int = Field(..., title="ID of characteristic")
-    product_id: int = Field(..., title="ID of required product")
+    id: int = Field(
+        ...,
+        title="ID of characteristic",
+        example=2
+    )
+    product_id: int = Field(
+        ...,
+        title="ID of required product",
+        example=1
+    )
 
     class Config:
         orm_mode = True
 
 class ProductBase(BaseModel):
-    name: str = Field(..., title="Name of product", description="Example: Laptop, toy, car, etc")
-    quantity: int = Field(..., title="Quantity of product", description="How much of this product we have")
-    sku: str = Field(..., title="SKU of product", description="Unique identifier for this product")
+    name: str = Field(
+        ...,
+        title="Name of product",
+        description="Name of requiered product",
+        example="Laptop"
+    )
+    quantity: int = Field(
+        ...,
+        title="Quantity of product",
+        description="How much of this product we have",
+        example=30
+    )
+    sku: str = Field(
+        ...,
+        title="SKU of product",
+        description="Unique identifier for this product",
+        example="LAP712"
+    )
 
 class ProductCreate(ProductBase):
     characteristics: List[CharacteristicCreate] = Field(
         default=[], title="Characteristics of product",
-        description="Additional info about product"
+        description="Additional info about product",
+        example="RAM"
     )
 
 class Product(ProductBase):
     id: int = Field(..., title="ID of product", description="Unique identifier for this product")
     characteristics: List[Characteristic] = Field(
         default=[], title="Characteristics of product",
-        description="Additional info about product"
+        description="Additional info about product",
+        example=1
     )
 
     class Config:
