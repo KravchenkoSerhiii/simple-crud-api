96d192e93410254204307913e8a8fe5248b455fa
serhii <admin@admin.com>
2025-03-23 13:20:52 +0100

    Changes with crud operations

--- crud/products.py
+++ crud/products.py
@@ -1,6 +1,7 @@
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.future import select
 from sqlalchemy.orm import selectinload
+import schemas
 
 import models
 import schemas
@@ -10,15 +11,15 @@ async def create_product(db: AsyncSession, product: schemas.ProductCreate):
     db.add(db_product)
     await db.commit()
     await db.refresh(db_product)
-    for characteristic in product.characteristics:
-        db_characteristic = models.Characteristic(
-            name=characteristic.name,
-            value=characteristic.value,
-            product_id=db_product.id,
-        )
-        db.add(db_characteristic)
-    await db.commit()
-    await db.refresh(db_product)
+    # for characteristic in product.characteristics:
+    #     db_characteristic = models.Characteristic(
+    #         name=characteristic.name,
+    #         value=characteristic.value,
+    #         product_id=db_product.id,
+    #     )
+    #     db.add(db_characteristic)
+    # await db.commit()
+    # await db.refresh(db_product)
     return db_product
 
 #Get all products
